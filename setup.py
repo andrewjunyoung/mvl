8b06952fbfe4cb06a4e584c50df3d420900dd334
andrewjunyoung <andrew.young16@imperial.ac.uk>
2020-01-07 22:49:36 +0000

    Updated version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name = 'mvl',
-    version = '0.1.0',
+    version = '0.2.0',
     author = 'Andrew J. Young',
     author_email = 'andrewjunyoung1@gmail.com',
     description = 'A package which implements various systems of n valued ' \
